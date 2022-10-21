ba03e7932a7ad2ec5a15503963a4e06f4fd70c54
David L. Inman <davidlevere@gmail.com>
2022-10-21 16:47:21 -0400

    flip constants

--- src/sdl/render.rs
+++ src/sdl/render.rs
@@ -1,5 +1,12 @@
 
+use std::ffi::c_int;
+
 pub const SDL_RENDERER_SOFTWARE : u32 = 0x01;
 pub const SDL_RENDERER_ACCELERATED : u32 = 0x02;
 pub const SDL_RENDERER_PRESENTVSYNC : u32 = 0x04;
 pub const SDL_RENDERER_TARGETTEXTURE : u32 = 0x08;
+
+
+pub const SDL_FLIP_NONE : c_int = 0x00;
+pub const SDL_FLIP_HORIZONTAL : c_int = 0x01;
+pub const SDL_FLIP_VERTICAL : c_int = 0x02;

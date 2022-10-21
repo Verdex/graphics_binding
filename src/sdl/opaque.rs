7c9395a3404ee95a22bec1b9dd477833a0130c01
David L. Inman <davidlevere@gmail.com>
2022-10-20 22:12:08 -0400

    fix opaque structs

--- src/sdl/opaque.rs
+++ src/sdl/opaque.rs
@@ -1,12 +1,28 @@
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct SDL_Window { }
+pub struct SDL_Window { 
+    _data : [u8; 0],
+    _marker : core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct SDL_Surface { }
+pub struct SDL_Surface { 
+    _data : [u8; 0],
+    _marker : core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct SDL_Texture { }
+pub struct SDL_Texture {
+    _data : [u8; 0],
+    _marker : core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+ }
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct SDL_Renderer { 
+    _data : [u8; 0],
+    _marker : core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
--- src/ttf/opaque.rs
+++ src/ttf/opaque.rs
@@ -2,4 +2,7 @@
 
 #[repr(C)]
 #[derive(Clone, Copy)]
-pub struct TTF_Font { }
+pub struct TTF_Font { 
+    _data : [u8; 0],
+    _marker : core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+}
